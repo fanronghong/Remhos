a0987b57203b83ee86a7757dbb634ea4d1e884ee
Arturo Vargas <vargas45@llnl.gov>
2020-05-15 13:50:36 -0700

    update set up script -TODO move to make

--- setupRemhos.sh
+++ setupRemhos.sh
@@ -2,10 +2,9 @@ cd ../ &&
 wget https://computing.llnl.gov/projects/hypre-scalable-linear-solvers-multigrid-methods/download/hypre-2.10.0b.tar.gz &&
 tar -zxvf hypre-2.10.0b.tar.gz &&
 mv hypre-2.10.0b hypre &&
-cd hypre/src/ &&
-./configure --disable-fortran &&
-make -j 3 &&
-cd ../.. &&
+cd hypre/src/ && mkdir build && cd build &&
+cmake -DCMAKE_INSTALL_PREFIX=$(pwd)/../../ ../ && make -j && make install &&
+cd ../../../ &&
 wget http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/OLD/metis-4.0.3.tar.gz &&
 tar -zxvf metis-4.0.3.tar.gz && cd metis-4.0.3 &&
 make -j 3 && cd .. &&
